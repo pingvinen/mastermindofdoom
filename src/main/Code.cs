c29c38ec1b3cb1c56439632345bb5dde4ea52fe5
Patrick Boisen <patrick@skarp.dk>
2017-11-04 17:18:19 +0100

    Add Code class
    It simply has "slots" with integer values. Whatever UI can then make each value a color or whatever.

---
+++ src/main/Code.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Pingvinen.MasterMindOfDoom
+{
+    public class Code
+    {
+        /// <summary>
+        /// The values making up the code
+        /// </summary>
+        public virtual List<int> Slots { get; set; } = new List<int>();
+    }
+}
