5c2ead0a0146bc9833d75a050fd96ff62b15f79b
Patrick Boisen <patrick@skarp.dk>
2017-11-04 22:51:53 +0100

    Add convenience Length and constructor
    The new constructor allows a user to provide the values for the code directly.

--- src/main/Code.cs
+++ src/main/Code.cs
@@ -4,9 +4,20 @@ namespace Pingvinen.MasterMindOfDoom
 {
     public class Code
     {
+        public Code()
+        {
+        }
+
+        public Code(params int[] values)
+        {
+            Slots.AddRange(values);
+        }
+        
         /// <summary>
         /// The values making up the code
         /// </summary>
         public virtual List<int> Slots { get; set; } = new List<int>();
+
+        public virtual int Length => Slots.Count;
     }
 }
