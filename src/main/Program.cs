c8a3eb14a437d5ecc2f8b4c5b9614a7efde29d88
Patrick Boisen <patrick@skarp.dk>
2017-11-04 16:43:27 +0100

    Initial solution and project setup

---
+++ src/main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pingvinen.MasterMindOfDoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
