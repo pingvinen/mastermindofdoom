eb99dec8f6220ed3dace68327715c4b856e79493
Patrick Boisen <patrick@skarp.dk>
2017-11-04 17:25:15 +0100

    Add CodeMaker with ability to generate a code

---
+++ src/main/CodeMaker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Pingvinen.MasterMindOfDoom
+{
+    /// <summary>
+    /// The "code maker", i.e. the entity responsible
+    /// for making up and knowing the code and providing
+    /// feedback on guesses made by the code breaker.
+    /// </summary>
+    public class CodeMaker
+    {
+        private readonly Randoom rng;
+
+        public CodeMaker(Randoom rng)
+        {
+            this.rng = rng;
+        }
+
+        /// <summary>
+        /// The generated code
+        /// </summary>
+        public virtual Code Code { get; set; }
+
+        /// <summary>
+        /// Generate the code
+        /// </summary>
+        /// <param name="length">How long the code should be</param>
+        /// <param name="numberOfOptions">How many possible values each slot can hove</param>
+        /// <exception cref="InvalidOperationException">Thrown if the instance already has a code</exception>
+        public virtual void GenerateCode(int length, int numberOfOptions)
+        {
+            if (Code != default(Code))
+            {
+                throw new InvalidOperationException("I already have a code!");
+            }
+
+            Code = new Code();
+            Code.Slots.AddRange(rng.GetSequence(length, 0, numberOfOptions));
+        }
+    }
+}
---
+++ src/unit/CodeMakerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using FakeItEasy;
+using Xunit;
+
+namespace Pingvinen.MasterMindOfDoom
+{
+    public class CodeMakerTests
+    {
+        private readonly CodeMaker maker;
+        private readonly Randoom randoom;
+
+        public CodeMakerTests()
+        {
+            randoom = A.Fake<Randoom>();
+
+            A.CallTo(() => randoom.GetSequence(A<int>._, A<int>._, A<int>._)).Returns(new List<int> { 4 });
+            
+            maker = new CodeMaker(randoom);
+        }
+
+        [Fact]
+        public void GenerateCode_throwsIfAlreadyHasACode()
+        {
+            maker.Code = new Code();
+
+            Assert.Throws<InvalidOperationException>(() => maker.GenerateCode(4, 6));
+        }
+
+        [Fact]
+        public void GenerateCode_usesRandoom_toGetValues()
+        {
+            maker.GenerateCode(15, 666);
+
+            A.CallTo(() => randoom.GetSequence(15, 0, 666)).MustHaveHappened();
+        }
+    }
+}
